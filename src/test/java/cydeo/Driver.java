8704a5c213d7f3556103395fb12a914343283ec3
dzeratemirati <dzeratemirati@gmail.com>
2022-07-16 17:26:02 -0400

    search tag is deleted

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -43,7 +43,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "52.90.101.317";
+                        String gridAddress = "54.175.89.158";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
